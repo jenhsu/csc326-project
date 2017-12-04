4052eaeb9b6d67f811af42e4f87c59caa67f6c51
Yu  Suyuan <yusuyuan@ug158.eecg.toronto.edu>
2017-12-04 16:47:12 -0500

    script update

--- aws_setup.py
+++ aws_setup.py
@@ -5,20 +5,16 @@ import paramiko
 import subprocess
 
 def setup_aws():
-    """connect to us-east-1, set up an instance,
-       associate Elastic IPs to it, copy files
-       to instance, and start server
+    """connect to us-east-1, set up an instance, 
+       and associate Elastic IPs to it
     """
-
-    # extract access info from crendential file
     f = open("credentials.csv", "r")
     line = f.readline()
     aws_access_key_id = line.split(",")[0].strip()
     aws_secret_access_key = line.split(",")[1].strip()
 
-    # setup connection and start instance
     conn = boto.ec2.connect_to_region(
-        'us-east-1',aws_access_key_id = aws_access_key_id,
+        'us-east-1',aws_access_key_id = aws_access_key_id, 
 	aws_secret_access_key = aws_secret_access_key)
     key_pair = conn.create_key_pair('scriptkey')
     key_pair.save("")
@@ -27,24 +23,21 @@ def setup_aws():
     security.authorize('tcp', 22, 22, '0.0.0.0/0')
     security.authorize('tcp', 80, 80, '0.0.0.0/0')
     reservation = conn.run_instances(
-        'ami-9aaa1cf2', key_name = 'scriptkey', instance_type = 't2.micro',
+        'ami-9aaa1cf2', key_name = 'scriptkey', instance_type = 't2.micro', 
         security_groups = ['csc326-group23'])
 
-    # wait until the instance is running
+    # give aws enough time to initialize the instance, otherwise allocate_address will return error
     while reservation.instances[0].update() != 'running':
         time.sleep(5)
-
-    # set up elastic ip
     address = conn.allocate_address()
     address.associate(reservation.instances[0].id)
     ip = address.public_ip
+    ip_http = "http://"+ip
     print ip
 
-    # get dns of the instance
     dns = conn.get_all_instances(instance_ids = [reservation.instances[0].id])[0].instances[0].public_dns_name
     print dns
 
-    # wait for server to be stable/reachable
     while True:
         istatus=conn.get_all_instance_status(instance_ids = [reservation.instances[0].id])
         print istatus[0].system_status.details["reachability"]
@@ -52,21 +45,17 @@ def setup_aws():
            break
         time.sleep(5)
 
-    # copy the required files in tar.gz format to the instance
     subprocess.check_call(["scp", "-i", "scriptkey.pem", "-o", "StrictHostKeyChecking=no", "-r", "csc326-project.tar.gz", "ubuntu@"+str(ip)+":~/"])
 
-    # set up ssh connection
     k = paramiko.RSAKey.from_private_key_file("scriptkey.pem") # must be in your current dir
     c = paramiko.SSHClient()
     c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     c.connect( hostname = str(ip), username = "ubuntu", pkey = k )
 
-    # the commands are the following: update apt-get, install pip, install python-dev and libev-dev for the bjoern library used for server,
-    # decompress the file copied earlier, install the libraries listed in requirements.txt, run frontend.py in the background
-    commands = [ "sudo apt-get update > /dev/null", "sudo apt-get install --yes python-pip > /dev/null", "sudo apt-get install --yes python-dev > /dev/null", "sudo apt-get install --yes libev-dev > /dev/null", "tar -xf csc326-project.tar.gz", "sudo pip install -r requirements.txt", "sudo nohup python frontend.py " + ip + " > /dev/null 2> frontend.err < /dev/null &"] # these commands will exec in series
+    commands = [ "sudo apt-get update > /dev/null", "sudo apt-get install --yes python-pip > /dev/null", "sudo apt-get install --yes python-dev > /dev/null", "sudo apt-get install --yes libev-dev > /dev/null", "tar -xf csc326-project.tar.gz", "sudo pip install -r requirements.txt", "sudo nohup python frontend.py " + ip_http + " > /dev/null 2> frontend.err < /dev/null &"] # these commands will exec in series
 
-    # excuting the commands and print the outputs
+
     for command in commands:
         print "Executing {}".format( command )
         stdin , stdout, stderr = c.exec_command(command) # this command is executed on the *remote* server
@@ -75,6 +64,7 @@ def setup_aws():
         print stderr.read()
 
     c.close()
+    return ip
 
 if __name__ == "__main__":
-    #setup_aws()
+    setup_aws()
